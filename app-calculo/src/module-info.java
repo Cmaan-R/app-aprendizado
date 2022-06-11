40769474f820de71e359aeddf52a6f5cbb3e5d68
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-11 17:10:18 -0300

    Update

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -6,8 +6,8 @@ module app.calculo {
 	requires transitive app.logging;
 	exports br.cmaan.app.calculo;
 	
-	exports br.cmaan.app.calculo.interno
-		to app.financeiro;
+//	exports br.cmaan.app.calculo.interno
+//		to app.financeiro;
 	
 	opens br.cmaan.app.calculo to app.financeiro;
 	
